5a7ec3ade5a8f8729c02da588055e799249f9ef2
wendy-silva <silvawendy0131@gmail.com>
2024-07-30 13:08:53 -0400

    commit

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // Here is where we import modules
 // We begin by loading Express
-import express from 'express'
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
 
 // server.js
 
@@ -11,6 +16,10 @@ app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
 });
